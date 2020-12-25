2586e141be5816dce9231875071d0214b77b5f6e
Justin Awrey <awreyjustin@gmail.com>
2020-12-25 13:37:34 -0800

    Pretty functional

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,5 +1,34 @@
 <script>
-  let hello = "Hello world!";
+  import Sveltree from "./Sveltree.svelte";
+
+  const leaves = [
+    {
+      title: "My life",
+      leaves: [
+        {
+          title: "Justin Awrey",
+          leaves: [
+            { title: "Is cool" },
+            { title: "Is so cool" },
+            { title: "supuer cool" },
+            { title: "you dont even know" },
+            { title: "Is cool2" },
+            { title: "Is so cool4", leaves: [{ title: "who knew" }] },
+            { title: "supuer cool9" },
+            { title: "you dont even 9know" },
+            { title: "Is coo1l" },
+            { title: "Is so 1cool" },
+            { title: "supuer -cool" },
+            { title: "you don0t even know" },
+          ],
+        },
+        {
+          title: "Paulina Buskas",
+          leaves: [{ title: "SUCKS", leaves: [{ title: "BIG TIME" }] }],
+        },
+      ],
+    },
+  ];
 </script>
 
-<p>{hello}</p>
+<Sveltree {leaves} />
